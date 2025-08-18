eef9620cc1174b6f122526c015c6c674d2fceb78
manukiran <manukiran.b@gmail.com>
2025-08-18 21:54:19 +0530

    this is my first commit

---
+++ src/test/java/tests/RegressionTest.java
@@ -0,0 +1,29 @@
+package tests;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+public class RegressionTest {
+
+	@Test
+	public static void regression_test_method() {
+		
+		System.out.println("this is regression test script");
+		
+	}
+	
+	@Test
+	public static void Regression_test_1()
+	{
+		System.out.println("Regression is done");
+	}
+	
+	@Test
+	public static void RegressionTest3() 
+	{
+		System.out.println("Regression is done 3rd time");
+	}
+	
+}
---
+++ src/test/java/tests/smokeTests.java
@@ -0,0 +1,26 @@
+package tests;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class smokeTests {
+
+	@Test
+	public static void smoke_test_method() {
+		
+		System.out.println("all are running through jenkins this is smoke test script");
+		
+	}
+	
+	@Test
+	public static void smoke_test_1()
+	{
+		System.out.println("Smoke is done");
+	}
+	
+	@Test
+	public static void RegressionTest3() 
+	{
+		System.out.println("all are running through jenkins  Smoke is done 3rd time");
+	}
+}
