928a73f23f3bbf110e031fa3595e8610ebfbbc0e
rkolamkar <rakshakolamkar218@gmail.com>
2025-08-19 15:35:23 +0530

    third commit

---
+++ src/test/java/tests/adhocTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class adhocTest {
+	
+	
+	@Test
+	public void adhocTest1() {
+		System.out.println("this is the 1st adhoc testcase");
+		
+	}
+	
+	@Test
+	public void adhocTest2() {
+		System.out.println("this is the 2nd adhoc testcase");
+		
+	}
+
+}
